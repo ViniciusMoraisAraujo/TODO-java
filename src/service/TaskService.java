cd641b7120986310a0cc03ef063bb72e7614742e
Vinicius Araujo <vinidev.morais@gmail.com>
2026-02-09 19:44:46 -0300

    feat: criado a classe TaskService

---
+++ src/service/TaskService.java
@@ -0,0 +1,74 @@
+package service;
+
+import model.Task;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class TaskService {
+    private List<Task> tasks;
+    public static final String FILE_NAME = "tasks.txt";
+
+    public TaskService() {
+        this.tasks = new ArrayList<>();
+        loadTask();
+    }
+
+    public void addTask(Task task) {
+        tasks.add(task);
+        Collections.sort(tasks);
+        saveTasks();
+    }
+
+    public void removeTask(String taskName) {
+        tasks.removeIf(task -> task.getName().equalsIgnoreCase(taskName));
+    }
+
+    public List<Task> getAllTasks() {
+        return tasks;
+    }
+
+    public List<Task> getTasksByCategory(String category) {
+        return tasks.stream()
+                .filter(task -> task.getCategory().equalsIgnoreCase(category))
+                .collect(Collectors.toList());
+    }
+
+    public List<Task> getTasksByStatus(String category) {
+        return tasks.stream()
+                .filter(task -> task.getCategory().equalsIgnoreCase(category))
+                .collect(Collectors.toList());
+    }
+
+    private void saveTasks() {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(FILE_NAME))) {
+            for (Task task : tasks) {
+                writer.write(task.toString());
+                writer.newLine();
+            }
+        } catch (IOException e) {
+            System.out.println("Error writing tasks to file" + e.getMessage());
+        }
+    }
+
+    private void loadTask() {
+        File file = new File(FILE_NAME);
+        if (!file.exists()) return;
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            String line;
+            while ((line = reader.readLine()) !=  null){
+                if (!line.trim().isEmpty()){
+                    tasks.add(Task.fromFileFormat(line));
+                }
+            }
+            Collections.sort(tasks);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+}
